45fb4fdc5354fccb745b0aca5f10a85d78e48169
ediez.online <info@ediez.online>
2022-09-14 17:28:25 +0100

    Reverted changes in _document.tsx as it is making the app not to work

--- apps/next/pages/_document.tsx
+++ apps/next/pages/_document.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
-// import { DocumentContext, DocumentInitialProps } from 'next/document'
-import { default as NativebaseDocument } from '@native-base/next-adapter/document'
+export { default } from '@native-base/next-adapter/document'
+// import React from 'react'
+// // import { DocumentContext, DocumentInitialProps } from 'next/document'
+// import { default as NativebaseDocument } from '@native-base/next-adapter/document'
 
-// Icon Font Library Imports
-import MaterialIconsFont from '@native-base/icons/FontsCSS/MaterialIconsFontFaceCSS'
-import EntypoFontFaceCSS from '@native-base/icons/FontsCSS/EntypoFontFaceCSS'
-const fontsCSS = `${MaterialIconsFont} ${EntypoFontFaceCSS}`
+// // Icon Font Library Imports
+// import MaterialIconsFont from '@native-base/icons/FontsCSS/MaterialIconsFontFaceCSS'
+// import EntypoFontFaceCSS from '@native-base/icons/FontsCSS/EntypoFontFaceCSS'
+// const fontsCSS = `${MaterialIconsFont} ${EntypoFontFaceCSS}`
 
-export default class Document extends NativebaseDocument {
-  static async getInitialProps(ctx: { renderPage: any }) {
-    const props = await super.getInitialProps(ctx)
-    const styles = [
-      <style key={'fontsCSS'} dangerouslySetInnerHTML={{ __html: fontsCSS }} />,
-      ...props.styles,
-    ]
-    return { ...props, styles: React.Children.toArray(styles) }
-  }
-}
+// export default class Document extends NativebaseDocument {
+//   static async getInitialProps(ctx: { renderPage: any }) {
+//     const props = await super.getInitialProps(ctx)
+//     const styles = [
+//       <style key={'fontsCSS'} dangerouslySetInnerHTML={{ __html: fontsCSS }} />,
+//       ...props.styles,
+//     ]
+//     return { ...props, styles: React.Children.toArray(styles) }
+//   }
+// }
